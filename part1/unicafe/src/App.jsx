28a1d5b608f1d9403cf1b06fecea0104490d0e5f
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-23 14:01:46 +0300

    1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,14 +12,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <h1>statistics</h1>
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good - bad) / (good + neutral + bad)}</p>
-        <p>positive {good / (good + neutral + bad)} %</p>
-      </div>
+      <table>
+        <tbody>
+          <tr><td>good</td><td>{good}</td></tr>
+          <tr><td>neutral</td><td>{neutral}</td></tr>
+          <tr><td>bad</td><td>{bad}</td></tr>
+          <tr><td>all</td><td>{good + neutral + bad}</td></tr>
+          <tr><td>average</td><td>{(good - bad) / (good + neutral + bad)}</td></tr>
+          <tr><td>positive</td><td>{good / (good + neutral + bad)} %</td></tr>
+        </tbody>
+      </table>
     </>
   )
 } 
