18e6ae1f4d1499eacb247cba8b6810abbd1f7847
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-26 10:37:33 +0300

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,7 @@
-const dummy = (blogs) => {
-    return 1
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, cur) => {return cur.likes + acc}, 0)
 }
 
 module.exports = {
-    dummy
+    totalLikes
 }
