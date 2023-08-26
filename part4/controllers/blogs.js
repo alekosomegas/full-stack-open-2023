6ad100a3b3406a257dc0ca406e809f8815899866
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-26 14:22:50 +0300

    4.8

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,33 +2,25 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const logger = require('../utils/logger')
 
-blogsRouter.get('/', (request, response, next) => {
-    Blog.find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-        .catch(error => logger.error(error))
+blogsRouter.get('/', async (request, response, next) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if(blog) {
-                response.json(blog)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => logger.error(error))
-})
-
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
     const blog = new Blog(request.body)
-    blog.save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => logger.error(error))
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+
+})
+
+blogsRouter.get('/:id', async (request, response, next) => {
+    const blog = await Blog.findById(request.params.id)
+    if(blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
 })
 
 module.exports = blogsRouter
---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+describe('when there is initially some notes saved', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.initialBlogs)
+    }, 100000)
+
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    }, 100000)
+
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    }, 100000)
+})
+
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
