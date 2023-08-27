6a51f10fceb053da5f0991cec8f94f6ca43a6ef1
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-27 17:56:26 +0300

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,32 +1,51 @@
-import React from 'react'
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import Blog from './Blog'
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Blog from "./Blog";
 
-test('renders content', () => {
+test("renders content", () => {
   const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'me'
-  }
+    title: "Component testing is done with react-testing-library",
+    author: "me",
+  };
 
-  render(<Blog blog={blog} />)
+  render(<Blog blog={blog} />);
 
-  const element = screen.getByText(/Component testing is done with react-testing-library/)
-  expect(element).toBeDefined()
-})
+  const element = screen.getByText(
+    /Component testing is done with react-testing-library/
+  );
+  expect(element).toBeDefined();
+});
 
-test('renders blog\'s title and author only', () => {
-    const blog = {
-        title: 'Only title',
-        author: 'Only author',
-        url: 'not displayed',
-      }
+test("renders blog's title and author only", () => {
+  const blog = {
+    title: "Only title",
+    author: "Only author",
+    url: "not displayed",
+  };
 
-    const container = render(<Blog blog={blog} />).container
+  const container = render(<Blog blog={blog} />).container;
 
-    const title = screen.getByText('Only title', {exact: false})
-    const author = screen.getByText('Only author', {exact: false})
-    const urlContainer = container.querySelector('#extraInfo')
-    expect(urlContainer).toHaveStyle('display: none')
-})
+  const title = screen.getByText("Only title", { exact: false });
+  const author = screen.getByText("Only author", { exact: false });
+  const urlContainer = container.querySelector("#extraInfo");
+  expect(urlContainer).toHaveStyle("display: none");
+});
+
+test("url and likes are shown after clicking show button", async () => {
+  const blog = {
+    title: "Only title",
+    author: "Only author",
+    url: "not displayed",
+  };
+
+  const user = userEvent.setup();
+  const container = render(<Blog blog={blog} />).container;
+
+  const viewBtn = screen.getByText("view");
+
+  await user.click(viewBtn)
+  const urlContainer = container.querySelector("#extraInfo");
+  expect(urlContainer).not.toHaveStyle("display: none");
+});
