5e0b80bbfec20d14fd1fa39088adcee0d7ad428d
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-29 12:48:18 +0300

    6.15

---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    return response.data
+}
+const addToDb = async (anecdote) => {
+    const response = await axios.post(baseUrl, anecdote)
+    return response
+}
+
+export default { getAll, addToDb }
