10bbbc25aa5ff9b4ee40eb8b668f33751572fca2
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-29 14:45:00 +0300

    6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,23 @@
+import { createAnecdote } from "../requests"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+
 const AnecdoteForm = () => {
 
+  const getId = () => (100000 * Math.random()).toFixed(0);
+ 
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+  
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, id: getId(), votes: 0 })
 }
 
   return (
